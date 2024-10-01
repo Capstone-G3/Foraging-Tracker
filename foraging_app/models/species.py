3176ae0e40924c32a79c83adf619d96872cc7178
henry <henry.retzer@gmail.com>
2024-10-01 15:56:30 -0500

    Created and added methods for User, Group, Markers, and other models relating to them.

---
+++ foraging_app/models/species.py
@@ -0,0 +1,29 @@
+from django.db.models import (Model, AutoField, CharField, ImageField)
+
+from foraging_app.models.marker import Marker
+
+
+class Species(Model):
+    id = AutoField(primary_key=True)
+    name = CharField(max_length=120, unique=True, null=False)
+    category = CharField(max_length=120, null=False)
+    scope = CharField(max_length=120, null=False)
+    description = CharField(max_length=512)
+    image = ImageField(upload_to=None, height_field=None, width_field=None, max_length=100, null=False)
+
+    def save(self, **kwargs):
+        super().save(**kwargs)
+
+    def delete(self, **kwargs):
+        super().delete(**kwargs)
+
+    def __str__(self):
+        return self.name
+
+    def getMarkers(self):
+        targetID = self.id
+        markerIDs = Marker.objects.filter(species=targetID).values_list('id', flat=True)
+        markers = []
+        for x in markerIDs:
+            markers.append(Marker.objects.get(id=x))
+        return markers
