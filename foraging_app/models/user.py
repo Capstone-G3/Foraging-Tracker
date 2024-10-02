35168a7a7af0d763478d4e08959c953d5a32fba1
danzzu <danhle346@gmail.com>
2024-10-01 23:06:51 -0500

    Refactor User Model

--- foraging_app/models/user.py
+++ foraging_app/models/user.py
@@ -1,44 +1,34 @@
 from django.db.models import (Model, AutoField, CharField, IntegerField, ForeignKey,
-                              CASCADE, DateField, EmailField, ImageField)
+                              CASCADE, DateField, ImageField)
 
 from django.contrib.auth.models import AbstractUser
+from django.core.mail import send_mail
 
-# class MyUserManager(BaseUserManager):
-#     def create_user(self, username, password, rating, badge, profile_image, created_since):
-#         if not username:
-#             raise ValueError('Users must have username')
-#         user = self.model(
-#             username=username,
-#             rating=rating,
-#             badge=badge,
-#             profile_image=profile_image,
-#             created_since=created_since,
-#         )
-#         user.set_password(password)
-#         user.save()
-#         return user
-    #def superuser?
+from foraging_app.settings import EMAIL_HOST_USER
 
 class User(AbstractUser):
+    """
+        Forage User Class with custom fields.
+    """
     BADGE = [("Diamond", 100000), ("Platinum", 10000), ("Gold", 1000), ("Silver", 100), ("Bronze", 10)]
-    USERNAME_FIELD = 'username'
 
-    id = AutoField(primary_key=True)
-    username = CharField(max_length=120, null=False, unique=True)
-    password = CharField(max_length=64, null=False)
+    id = AutoField(primary_key=True, editable=False)
     rating = IntegerField(default=0)
     profile_image = ImageField(upload_to='static/images', height_field=None, width_field=None, max_length=100, null=False)
     created_since = DateField(auto_now=True, null=False)
 
     def __str__(self):
         return self.username
+    
+    def email_user(self, subject, message, **extra):
+        """
+            Send Email from the application to the User's given email.
+        """
+        send_mail(subject,message,EMAIL_HOST_USER,[self.email], **extra)
 
 
-class User_Profile(Model):
+class UserProfile(Model):
     id = AutoField(primary_key=True)
-    first_name = CharField(max_length=120, null=False)
-    last_name = CharField(max_length=120, null=False)
-    email = EmailField(max_length=254, null=False)
     home_address = CharField(max_length=254, null=False)
     phone = CharField(max_length=15, default=None)
     gender = [("Male", 2), ("Female", 1), ("Other", 9)]
