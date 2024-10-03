341064e52ffcdea7f75d4d989470b30177860b49
danzzu <danhle346@gmail.com>
2024-10-03 03:17:15 -0500

    Moved to appropriate relations

--- foraging_app/models/group.py
+++ foraging_app/models/group.py
@@ -1,14 +1,15 @@
-from django.db.models import Model, AutoField, CharField, ForeignKey, CASCADE
+from django.db.models import Model, AutoField, CharField, ForeignKey, CASCADE, SET_NULL
 
-from foraging_app.models import user
-
+from foraging_app.models.user import User
+from foraging_app.models.marker import Marker
 
 class Group(Model):
+
     id = AutoField(primary_key=True)
     name = CharField(max_length=120, unique=True, null=False)
     category = CharField(max_length=120, null=False)
     description = CharField(max_length=512)
-    user_admin = ForeignKey(user.User, on_delete=CASCADE, null=False)
+    user_admin = ForeignKey(User, on_delete=CASCADE, null=False)
 
 
     def save(self, **kwargs):
@@ -17,8 +18,39 @@ class Group(Model):
     def delete(self, **kwargs):
         super().delete(**kwargs)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.name
 
-    def getAdmin(self) -> user.User:
+    def getAdmin(self):
         return self.user_admin
+    
+
+class User_Group(Model):
+
+    user_id = ForeignKey(User, on_delete=SET_NULL, null=True)
+    group_id = ForeignKey(Group, on_delete=CASCADE)
+
+    def getGroupMembers(self, targetGroup):
+        target_id = targetGroup.id
+        members_ids = User_Group.objects.filter(group_id=target_id).values_list('user_id', flat=True)
+        members = []
+        for x in members_ids:
+            members.append(User.objects.get(id=x))
+        return members
+
+    def getGroupMarkers(self, targetGroup):
+        target_id = targetGroup.id
+        member_ids = User_Group.objects.filter(group_id=target_id).values_list('user_id', flat=True)
+        members = []
+        for x in member_ids:
+            members.append(User.objects.get(id=x))
+        marker_owners = []
+        markers = []
+        for x in members:
+            marker_owners = Marker.objects.filter(owner=x).values_list('id', flat=True)
+            for y in marker_owners:
+                markers.append(Marker.objects.get(id=y))
+        return markers
+
+    def existInGroup(self, member, targetGroup):
+        return User_Group.objects.filter(group_id=targetGroup.id, user_id=member.id).exists()
--- foraging_app/models/user.py
+++ foraging_app/models/user.py
@@ -1,31 +1,34 @@
-from django.db.models import (AutoField, IntegerField, CASCADE, DateField, ImageField)
-from foraging_app.models import group
-from foraging_app.models.user_group import User_Group
-
+from django.db.models import (AutoField, IntegerField, DateField, CharField, ImageField, ForeignKey, Model, CASCADE)
 from django.contrib.auth.models import AbstractUser
 from django.core.mail import send_mail
-
 from foraging_app.settings import EMAIL_HOST_USER
 
 class User(AbstractUser):
     """
         Forage User Class with custom fields.
     """
-    BADGE = [("Diamond", 100000), ("Platinum", 10000), ("Gold", 1000), ("Silver", 100), ("Bronze", 10)]
+    BADGE = (
+        ("Diamond", 100000),
+        ("Platinum", 10000),
+        ("Gold", 1000),
+        ("Silver", 100),
+        ("Bronze", 10)
+    )
 
     id = AutoField(primary_key=True, editable=False)
     rating = IntegerField(default=0)
     profile_image = ImageField(upload_to='static/images', height_field=None, width_field=None, max_length=100, null=False)
     created_since = DateField(auto_now=True, null=False)
+    # user_manager = User_Manager()
 
     def __str__(self):
         return self.username
     
-    def email_user(self, subject, message, **extra):
-        """
-            Send Email from the application to the User's given email.
-        """
-        send_mail(subject,message,EMAIL_HOST_USER,[self.email], **extra)
+    # def email_user(self, subject, message, **extra):
+    #     """
+    #         Send Email from the application to the User's given email.
+    #     """
+    #     send_mail(subject,message,EMAIL_HOST_USER,[self.email], **extra)
 
     def save(self, **kwargs):
         super().save(**kwargs)
@@ -34,27 +37,47 @@ class User(AbstractUser):
         super().delete(**kwargs)
 
     def setProfileByField(self, **kwargs):
-        if 'username' in kwargs:
-            self.username = kwargs['name']
-        if 'password' in kwargs:
-            self.password = kwargs['password']
         if 'rating' in kwargs:
             self.rating = kwargs['rating']
         if 'profile_image' in kwargs:
             self.profile_image = kwargs['profile_image']
         self.save()
 
-    def setPassword(self, password: str):
-        self.password = password
-        self.save()
-
+    def getName(self):
+        return self.username
+    
     def getGroups(self):
+        from foraging_app.models.group import Group, User_Group
         targetID = self.id
         groupIDs = User_Group.objects.filter(user_id=targetID).values_list('group_id', flat=True)
         groups = []
         for x in groupIDs:
-            groups.append(group.Group.objects.get(id=x))
+            groups.append(Group.objects.get(id=x))
         return groups
+    
 
-    def getName(self):
-        return self.username
+class User_Profile(Model):
+    MALE=2
+    FEMALE=1
+    OTHER=9
+
+    GENDER = (
+        ("Male", 2), 
+        ("Female", 1), 
+        ("Other", 9)
+    )
+    
+    id = AutoField(primary_key=True)
+    home_address = CharField(max_length=254, null=False)
+    phone = CharField(max_length=15, default=None)
+    gender= IntegerField(choices=GENDER,default=MALE)
+    user_id= ForeignKey(User, on_delete=CASCADE)
+
+    def get_home_address(self):
+        return self.home_address
+    
+    def get_phone(self):
+        return self.phone
+    
+    def get_gender(self):
+        return self.gender
