4b0cd818da6a77244bcd6083ba3cde0c8443dbde
ivanjaviersosa <javiers3@gmail.com>
2024-10-04 15:42:10 -0500

    Logout implementation: url path, view, html button Tests implementation/refactor: Marker, User, Group, LikeCount, Species

--- foraging_app/tests/unit/test_friend.py
+++ foraging_app/tests/unit/test_friend.py
@@ -23,4 +23,33 @@ class TestFriendshipModel(TestCase):
     #     self.assertEqual(friend1.user_a, self.test_user1)
     #     self.assertEqual(friend1.user_b, self.test_user2)
     #     self.assertIsNotNone(friend1.saved_date)
+
+    #   TODO UN-COMMENT  WHEN FRIEND REQUEST IS IMPLEMENTED
+    #   test friend request has a valid sending user as well as receiving user
+    #   test status of request is not empty/null
+    #   test request date of request is not empty/null
+    # def test_friend_request(self):
+    #     test_user1 = User.objects.create(id=2345, username='Bob', password='123', rating=0,
+    #                                      created_since=date.today())
+    #     test_user2 = User.objects.create(id=5265, username='Alice', password='000', rating=0,
+    #                                      created_since=date.today())
+    #     friend_request1 = Friend_Request.objects.create(uid_sender=test_user1, uid_reciever=test_user2,
+    #                                                     status='Accept', request_date=date.today())
+    #     self.assertEqual(friend_request1.uid_sender, test_user1)
+    #     self.assertEqual(friend_request1.uid_reciever, test_user2)
+    #     self.assertIsNotNone(friend_request1.status)
+    #     self.assertIsNotNone(friend_request1.request_date)
+
+    #   **********UN-COMMENT  WHEN FRIEND IS IMPLEMENTED**********
+    #   test friend model field user_a and user_b correspond to user and the user that was added as a friend
+    # def test_friend(self):
+    #     test_user1 = User.objects.create(id=1111, username='Bob', password='123', rating=0,
+    #                                      created_since=date.today())
+    #     test_user2 = User.objects.create(id=2314, username='Alice', password='000', rating=0,
+    #                                      created_since=date.today())
+    #     friend1 = Friend.objects.create(user_a=test_user1, user_b=test_user2, saved_date=date.today())
+    #
+    #     self.assertEqual(friend1.user_a, test_user1)
+    #     self.assertEqual(friend1.user_b, test_user2)
+    #     self.assertIsNotNone(friend1.saved_date)
     pass
--- foraging_app/views/logout.py
+++ foraging_app/views/logout.py
@@ -1,10 +1,14 @@
-from django.contrib.auth import logout
-from django.shortcuts import redirect
+from django.contrib.auth import logout, authenticate
+from django.core.checks import messages
+from django.shortcuts import redirect, render
 from django.views import View
+from django.contrib import messages
 
 
 class Logout_View(View):
-
+    def get(self, request):
+        return render(request, "logout.html")
     def post(self, request):
         logout(request)
-        return redirect('')
+        messages.success(request, "You have been logged out.")
+        return redirect('home')
