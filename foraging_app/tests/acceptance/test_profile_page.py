be2d5bb21f248c6ab6dce315af7ed31169a9e8a3
ivanjaviersosa <javiers3@gmail.com>
2024-10-10 17:22:43 -0500

    acceptance test for profile page updates

--- foraging_app/tests/acceptance/test_profile_page.py
+++ foraging_app/tests/acceptance/test_profile_page.py
@@ -1,8 +1,9 @@
 from datetime import datetime
-
 from django.test import TestCase, Client
 from django.urls import reverse
-from foraging_app.models import User, Group
+from foraging_app.models import User, Group, Marker, Species
+from foraging_app.models.group import User_Group
+from foraging_app.models.marker import User_Marker
 from foraging_app.models.user import User_Profile
 import os
 import tempfile
@@ -15,17 +16,26 @@ class ProfilePageTest(TestCase):
                                              profile_image='test.png')
         self.user_profile = User_Profile.objects.create(home_address='123 S Ave', phone='123 456 789', gender=1,
                                                         user_id=self.user)
-        self.user.friend
         self.login_url = reverse('login')
         self.home_url = reverse('home')
         self.profile_url = reverse('profile')
         self.client.post(self.login_url, {'username': self.user.username, 'password': '12345'})
-        # self.assertTrue(self.client.login(username='test', password='<PASSWORD>'))
+        self.species1 = Species.objects.create(id=123, name='Species 1', category='fruit'
+                                               , scope='testscope', description='Found berry', image='/testImagePath')
+        self.marker1 = Marker.objects.create(id=0000, title='Lion''s mane', latitude=43.07866235729638,
+                                             longitude=-87.881974725981,
+                                             is_private=False, species=self.species1, owner=self.user)
+        self.user_marker1 = User_Marker.objects.create(user_id=self.user, marker_id=self.marker1,
+                                                       saved_date=datetime.now())
+        self.group1 = Group.objects.create(name='Group 1')
+        self.user_group1 = User_Group.objects.create(group=self.group1, user=self.user)
 
+    # test user can reach their profile page
     def test_can_reach_profile_page(self):
         response = self.client.post(reverse('profile', args=self.user_profile.id))
         self.assertEqual(response.status_code, 200)
 
+    #   test user can edit their profile page
     def test_edit_profile_page(self):
         self.client.post(reverse('profile', args=self.user_profile.id))
         testImagePath = tempfile.NamedTemporaryFile(suffix=".png").name
@@ -54,9 +64,11 @@ class ProfilePageTest(TestCase):
         self.assertEqual(response.status_code, 200)
         os.remove(testImagePath)
 
+    #   test user can manage(accept/remove) their friends on their profile page
     def test_profile_manage_friends(self):
         user2 = User.objects.create_user(username='user2', email='user2@gmail.com', password='0000', )
         user3 = User.objects.create_user(username='user3', email='user3@gmail.com', password='1111', )
+        #TODO test will run after friend/friendship implementation
         friendship = Friend.objects.create(user_a=self.user, user_br=user2, friend_since=datetime.now())
         friend_request = Friend_Request.objects.create(uid_sender=user3, uiweweweweed_receiver=self.user, status=1,
                                                        request_date=datetime.now())
@@ -64,75 +76,76 @@ class ProfilePageTest(TestCase):
         self.client.post(reverse('profile', args=self.user_profile.id))
         response = self.client.post(reverse('friends'))
         self.assertEqual(response.status_code, 200)
-        #TODO figure out if the view for managing friends would be a single view that handles accepting and rejecting request
+        # TODO figure out if the view for managing friends would be a single view that handles accepting and
+        #  rejecting request
         self.client.post(reverse('manage_friend_request', args=user3), {'friend_request': 'accept'})
         self.assertEqual(friend_request.status, 1)
-        # add a test case to see friends list
 
+        response = self.client.post(reverse('manage_friends', args=user3), {'remove_friend': True})
+        self.assertEqual(response.status_code, 200)
+
+    #   test user can view their posts on their profile page
     def test_see_own_posts(self):
-        pass
+        response = self.client.get(reverse('profile', args=self.user.id))
+        self.assertContains(response,
+                            self.user_marker1.marker_id)  # double check if the marker id is way to recognize the
+        # post on page
 
+    #   test user can delete their posts from their profile page
     def test_delete_my_posts(self):
-        pass
+        self.client.get(reverse('profile', args=self.user.id))
+        response = self.client.post(reverse('delete_marker', args=self.marker1.id))
+        self.assertEqual(response.status_code, 200)
 
+        response = self.client.get(reverse('profile', args=self.user.id))
+        self.assertNotContains(response, self.user_marker1.marker_id)
+
+    #   test user can create posts from their profile
     def test_create_new_post(self):
-        pass
+        marker = {'title': 'test marker', 'latitude': 43.07866235729638, 'longitude': -87.881974725981,
+                  'is_private': False, 'species': self.species1}
+        response = self.client.post(reverse('create_marker'), marker)
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get(reverse('profile', args=self.user.id))
+        self.assertContains(response, 'test marker')
 
+    #   test user can create a group from their profile
+    def test_create_group(self):
+        self.client.post(reverse('profile', args=self.user_profile.id))
+        self.client.post(reverse('create_group'))
+        group = {'name': 'test group', 'description': 'group description', 'category': 'mushrooms', 'is_private': False}
+        response = self.client.post(reverse('create_group'), {group})
+        self.client.post(reverse('profile', args=self.user_profile.id))
+        self.assertContains(response, 'test group')
+
+    #   test user can view groups from their profile
     def test_view_groups(self):
-        pass
+        response = self.client.post(reverse('profile', args=self.user.id))
+        self.assertContains(response, self.group1.id)
 
+    #   test user can reach their settings to edit their account settings
     def test_adjust_profile_settings(self):
-        pass
+        response = self.client.post(reverse('profile', args=self.user.id))
+        self.assertContains(response, 'settings')
+        response = self.client.post(reverse('settings', args=self.user.id))
+        self.assertRedirects(response, reverse('settings', args=self.user.id), status_code=302, )
 
-    def test_delete_account(self):
-        pass
-
+    #   test user can view other's posts from their profiles
     def test_can_view_others_posts_on_their_profiles(self):
-        pass
+        user2 = User.objects.create_user(username='test2', email='test2@gmail.com', password='1111',
+                                         profile_image='test.png')
+        User_Profile.objects.create(home_address='123 S Ave', phone='123 456 789', gender=1,
+                                    user_id=user2)
+        marker2 = Marker.objects.create(id=111, title='Lion''s mane', latitude=43.07866235729638,
+                                        longitude=-87.881974725981,
+                                        is_private=False, species=self.species1, owner=user2)
+        self.user_marker2 = User_Marker.objects.create(user_id=user2, marker_id=marker2,
+                                                       saved_date=datetime.now())
+        response = self.client.post(reverse('profile', args=user2.id))
+        self.assertEqual(response.status_code, 302)
+        self.assertContains(response, 'Lion''s mane')
 
-    def test_can_view_others_friends_list(self):
-        pass
-
+    #test user can view their badge levvel on their profile
     def test_can_view_badge_level(self):
-        pass
-
-    def test_can_navigate_to_other_pages(self):
-        pass
-
-# class GroupsPageTest(TestCase):
-#     def setup(self):
-#         self.client = Client()
-#         self.user = User.objects.create_user(username='test', email='test@gmail.com', password='12345',
-#                                              profile_image='test.png')
-#         self.user_profile = User_Profile.objects.create(home_address='123 S Ave', phone='123 456 789', gender=1,
-#                                                         user_id=self.user)
-#         self.login_url = reverse('login')
-#         self.home_url = reverse('home')
-#         self.client.post(self.login_url, {'username': self.user.username, 'password': '12345'})
-#
-#     def handleSetup(self, isAdmin):
-#         # group = Group.objects.create(name='test', description='test')
-#         if isAdmin:
-#
-#             group = Group.objects.create(name='test_group', description='test', user_admin=self.user)
-#             return group
-#
-#         else:
-#             user2 = User.objects.create_user(username='test2', email='test2@gmail.com', password='1111',
-#                                              profile_image='test.png')
-#             group = Group.objects.create(name='test_group', description='test', user_admin=user2)
-#             return group
-#
-#     def test_request_to_join(self):
-#         admin = False
-#         group = self.handleSetup(admin)
-#
-#         response = self.client.post(reverse('join_group', args=[group.id]))
-#         self.assertTrue(response.status_code == 200)
-#
-#     def test_can_edit_groups(self):
-#         admin = True
-#         group = self.handleSetup(admin)
-#         # .... make test for this case
-#
-#     def test_get
+        response = self.client.post(reverse('profile', args=self.user.id))
+        self.assertContains(response, 'Bronze')
