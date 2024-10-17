43c0681c5baea2cd56ccda0500b28bc055ab8a7d
danzzu <danhle346@gmail.com>
2024-10-16 23:29:46 -0500

    Make sure if user is already logged in, they don't have to encounter another login request.

--- foraging_app/views/logout.py
+++ foraging_app/views/logout.py
@@ -3,9 +3,13 @@ from django.core.checks import messages
 from django.shortcuts import redirect, render
 from django.views import View
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class Logout_View(View):
+class Logout_View(LoginRequiredMixin ,View):
+    login_url = '/login/'
+    redirect_field_name = 'login'
+    
     def post(self, request):
         logout(request)
         return redirect('home')
