a81805224f00c753b5360ec576eebcb3b75b16b5
jjustus19 <jsjustus@uwm.edu>
2024-10-17 01:22:39 -0500

    categories page plus modification of index and added navbar

--- foraging_app/views/home.py
+++ foraging_app/views/home.py
@@ -7,7 +7,8 @@ class Home_View(View):
         self.figure = DesktopMap().compile_figure()
 
     def get(self,request):
-        return render(request, "index.html", {"map" : self.figure , "username" : "Log In"})
+        username = request.user.username if request.user.is_authenticated else "Log In"
+        return render(request, "index.html", {"map" : self.figure , "username" : username})
 
 class About_Us_View(View):
     def get(self, request):
