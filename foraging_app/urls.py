16cc120fa38d729ffdc82b0a069b488dea121bb5
danzzu <danhle346@gmail.com>
2024-10-17 17:20:40 -0500

    Required Django Settings

--- foraging_app/urls.py
+++ foraging_app/urls.py
@@ -21,7 +21,10 @@ from foraging_app.views.home import Home_View, About_Us_View
 from foraging_app.views.login import Login_View
 from foraging_app.views.logout import Logout_View
 from foraging_app.views.registration import Register_View
-from django.core.mail import send_mail
+from foraging_app.views.marker import Marker_Create_View, Marker_Edit_View, Marker_Home_View, Marker_Delete_View, Marker_Details_View
+from foraging_app import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -39,4 +42,13 @@ urlpatterns = [
     #Logout url
     path('logout/', Logout_View.as_view(), name="logout"),
 
-]
+
+    # Marker urls
+    path('markers/', Marker_Home_View.as_view(), name='home_marker'),
+    # TODO : Add dynamic marker for onsite form.
+    path('marker/create', Marker_Create_View.as_view(), name="create_marker"),
+    path('marker/<int:marker_id>', Marker_Details_View.as_view(), name='info_marker'),
+    path('marker/<int:marker_id>/edit', Marker_Edit_View.as_view(), name='edit_marker'),
+    path('marker/<int:marker_id>/edit/delete', Marker_Delete_View.as_view(), name='delete_marker'),
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # Remove for production.
