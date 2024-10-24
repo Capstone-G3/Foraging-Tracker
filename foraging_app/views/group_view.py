d053fecbfe3fd0799192f11cbc86bab7648bf2f1
henry <henry.retzer@gmail.com>
2024-10-24 18:31:18 -0500

    Created group acceptance tests and group page / view along with group creation page

--- foraging_app/views/group_view.py
+++ foraging_app/views/group_view.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.urls import reverse
+from oauthlib.openid.connect.core.exceptions import LoginRequired
+
 from foraging_app.forms.group import GroupCreateForm
 from foraging_app.models.group import Group, User_Group
 from foraging_app.forms import group
@@ -9,7 +12,8 @@ from foraging_app.models.user import User
 from django.contrib.auth.hashers import make_password
 
 
-class Group_View(View):
+class Group_View(LoginRequiredMixin,View):
+    login_url = '/login/'
 
     def get(self, request, thisGroup=None):
         # thisGroup = Group.objects.filter(name="test1")
@@ -23,7 +27,7 @@ class Group_View(View):
                                               'members': members,
                                               'markers': markers})
 
-class Create_Group_View(View):
+class Create_Group_View(LoginRequiredMixin,View):
 
     def get(self, request):
         form = GroupCreateForm()
@@ -35,6 +39,7 @@ class Create_Group_View(View):
         if form.is_valid():
             data = form.cleaned_data
             group_create = Group.objects.create(**data, user_admin=request.user)
+            User_Group.objects.create(group_id=group_create, user_id=request.user)
             if group_create is not None:
                 messages.success(request,"Group creation complete.")
                 members = []
