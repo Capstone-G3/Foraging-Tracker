8046a64076eb97cf41f7598e823a306339798e3d
danzzu <danhle346@gmail.com>
2024-10-24 18:54:14 -0500

    Added User Profile into admin page

--- foraging_app/admin.py
+++ foraging_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from foraging_app.models.user import User
+from foraging_app.models.user import User, User_Profile
 from foraging_app.models.group import Group
 from foraging_app.models.marker import Marker
 from foraging_app.models.species import Species
@@ -9,3 +9,4 @@ admin.site.register(User)
 admin.site.register(Marker)
 admin.site.register(Group)
 admin.site.register(Species)
+admin.site.register(User_Profile)
