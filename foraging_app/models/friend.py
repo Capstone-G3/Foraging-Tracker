9f97ac37a940b78fce101c2d3faaa8e7a98a1a60
ivanjaviersosa <javiers3@gmail.com>
2024-11-03 23:35:40 -0600

    Friends model test and user profile page acceptance refactor.Friend and Friend_Request model n functions implementation

--- foraging_app/models/friend.py
+++ foraging_app/models/friend.py
@@ -1,19 +1,49 @@
-from django.db.models import (Model, AutoField, CharField, IntegerField, DateField, ImageField, BooleanField,
-                              ForeignKey, CASCADE, SET_NULL, DateTimeField, FloatField, TextField, Q)
+from django.db.models import (Model, IntegerField,
+                              ForeignKey, CASCADE, DateTimeField, OneToOneField,
+                              ManyToManyField)
 
 from foraging_app.models.user import User
 
 
 class Friend(Model):
-    user_a = ForeignKey(User, on_delete=CASCADE, related_name='friend_a')
-    user_b = ForeignKey(User, on_delete=CASCADE, related_name='friend_b')
+    user = OneToOneField(User, on_delete=CASCADE, related_name='user')
+    friends = ManyToManyField(User, blank=True, related_name='friends')
     friend_since = DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.user_b.username
+        return self.user.username
 
-    def get_friends(self):
-        return Friend.objects.filter(user_a=self).select_related('user_b')
+    def add_friend(self, account):
+        """
+        Adds a friend, if relationship doesn't exist yet
+        """
+        if account not in self.friends.all():
+            self.friends.add(account)
+            self.save()
+
+    def remove_friend(self, account):
+        """
+        Removes a friend, if relationship exists // core functionality
+        """
+        if account in self.friends.all():
+            self.friends.remove(account)
+            self.save()
+
+    def unfriend(self, account):
+        """
+        Initiate the action of unfriending users // logic check
+        """
+        self.remove_friend(account)  # remove a user from the remover's friends list
+        friends_list = Friend.objects.filter(user=account)  # query friends list of user being removed
+        for friend in friends_list:  # loop through that query to find user
+            friend.remove_friend(self.user)  # remove user from person being removed's friends list
+
+    def is_friend(self, account):
+        """
+        Checks if a friend is friend
+        """
+        if account in self.friends.all():  # check account (user 2) is friends with user 1 return true is so
+            return True
 
 
 class Friend_Request(Model):
@@ -24,5 +54,32 @@ class Friend_Request(Model):
     )
     uid_sender = ForeignKey(User, on_delete=CASCADE, related_name='request_sender')
     uid_receiver = ForeignKey(User, on_delete=CASCADE, related_name='request_receiver')
-    status = IntegerField(choices=Status, default=1)
-    request_date = DateTimeField(auto_now=True)
+    status = IntegerField(choices=Status, null=False, default=1)
+    request_date = DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.uid_sender.username
+
+    def accept(self):
+        """"
+        Accept a friend request
+        update friends list for both sender and receiver
+        """
+        receiver_friend_list = Friend.objects.get(user=self.uid_receiver)  # query receiver's friend list
+        if receiver_friend_list:  # if receiver friend list exists
+            receiver_friend_list.add_friend(self.uid_sender)  # add sender to receiver's friend list
+            sender_friend_list = Friend.objects.get(user=self.uid_sender)  # query sender's friend list
+            if sender_friend_list:  # if that friend exists
+                sender_friend_list.add_friend(self.uid_receiver)  # add receiver to sender's friend list
+                self.status = 0  # update F.R. status to 0 (aka accepted)
+                self.save()
+                return True
+
+    def reject(self):
+        """"
+        Reject a friend request
+        """
+        self.status = 2  # update F.R. status to 2 (aka rejected)
+        self.save()
+
+    # DO WE WANT TO ADD A CANCEL FEATURE FOR FRIEND REQUEST'S SENT?
--- foraging_app/tests/unit/test_friend.py
+++ foraging_app/tests/unit/test_friend.py
@@ -15,34 +15,76 @@ class TestFriendshipModel(TestCase):
         self.user3 = User.objects.create(username='User3', password=1545684324, rating=0,
                                          created_since=date.today())
 
+        self.friend1 = friend.Friend.objects.create(user=self.user1)
+        self.friend2 = friend.Friend.objects.create(user=self.user2)
+        self.friend3 = friend.Friend.objects.create(user=self.user3)
+        self.friend_request = friend.Friend_Request(uid_sender=self.user2, uid_receiver=self.user1,
+                                                    status=1, request_date=date.today())
+
     #   test friend request has a valid sending user as well as receiving user
-    #   test status of request is not empty/null
+    #   test status of request is not empty/null on default
     #   test request date of request is not empty/null
     def test_friend_request(self):
-        # friend1 = models.FRIEND(USER_A=self.Test_User1, USER_B=self.Test_User2, SAVED_DATE=date.today())
         friend_request1 = friend.Friend_Request(uid_sender=self.user1, uid_receiver=self.user2,
-                                                status=1, request_date=date.today())
+                                                request_date=date.today())
         self.assertEqual(friend_request1.uid_sender, self.user1)
         self.assertEqual(friend_request1.uid_receiver, self.user2)
         self.assertIsNotNone(friend_request1.status)
         self.assertIsNotNone(friend_request1.request_date)
         self.assertEqual(friend_request1.status, 1)
 
-    #   test friend model field user_a and user_b correspond to user and the user that was added as a friend
-    def test_friend(self):
-        friend1 = friend.Friend(user_a=self.user1, user_b=self.user2, friend_since=date.today())
-        friend.Friend(user_a=self.user1, user_b=self.user3, friend_since=date.today())
-        self.assertEqual(friend1.user_a, self.user1)
-        self.assertEqual(friend1.user_b, self.user2)
-        self.assertIsNotNone(friend1.friend_since)
-        self.assertEqual(friend1.friend_since, date.today())
+    #   test friend model function add_friend(), correctly adds user 2 into user 1's friends list
+    def test_add_friend(self):
+        self.friend1.add_friend(self.user2)
+        self.assertIn(self.user2, self.friend1.friends.all())
 
-        friends = friend.Friend.get_friends(self.user1)
-        self.assertEqual(len(friends), 2)
+    #   test friend model function remove_friend(), removes user 2 from user 1's friends list
+    def test_remove_friend(self):
+        self.friend2.add_friend(self.user1)
+        self.friend1.remove_friend(self.user2)
+        self.assertNotIn(self.user2, self.friend1.friends.all())
 
-    def test_friendship(self):
-        friend_request1 = friend.Friend_Request(uid_sender=self.user1, uid_receiver=self.user2,
-                                                status=1, request_date=date.today())
-        self.assertEqual(friend_request1.status, 1)
-        friend_request1.status = 2
-        self.assertEqual(friend_request1.status, 2)
+    # test friend model function unfriend removes user 2 from user 1's friend's list, as well as user 1 from user 2's -
+    # friends list
+    def test_unfriend(self):
+        self.friend1.add_friend(self.user2)
+        self.friend2.add_friend(self.user1)
+        self.friend1.unfriend(self.user2)
+        self.assertNotIn(self.user2, self.friend1.friends.all())
+        self.assertNotIn(self.user1, self.friend2.friends.all())
+
+    # test friend model function is_friend() returns True if user 2 is in user 1's friends list after being added,
+    # and vice versa. PRE CHECK is_friend() returns False for user 2 being in user 1's friend's list before
+    # being added as a friend and vice versa
+    def test_is_friend(self):
+        self.assertFalse(self.friend1.is_friend(self.user2))
+
+        self.friend1.add_friend(self.user2)
+        self.assertTrue(self.friend1.is_friend(self.user2))
+
+        self.assertFalse(self.friend2.is_friend(self.user1))
+
+        self.friend2.add_friend(self.user1)
+        self.assertTrue(self.friend2.is_friend(self.user1))
+
+    # test friend_request model's function accept() adds sender to receiver's friends list.
+    # test friend_request model field status updates accordingly when a user (user1) accepts a friend
+    # request from a sender (user2).
+    def test_accept_friend_request(self):
+        self.assertEqual(self.friend_request.status, 1)
+
+        isAccepted = self.friend_request.accept()
+        self.assertEqual(isAccepted, True)
+        self.assertEqual(self.friend_request.status, 0)
+
+        self.assertIn(self.user2, self.friend1.friends.all())
+
+    # test friend_request model function reject() sets status to 2.
+    # test reject() in fact does indeed not add sender (user1) to receiver's (user1)  friends list
+    def test_reject_friend_request(self):
+        self.assertEqual(self.friend_request.status, 1)
+
+        self.friend_request.reject()
+        self.assertEqual(self.friend_request.status, 2)
+
+        self.assertNotIn(self.user2, self.friend1.friends.all())
