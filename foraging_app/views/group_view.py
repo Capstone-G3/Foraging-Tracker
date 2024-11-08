90c1d6f246ac4b4bc379d13a11012f0f13d5aef1
henry <henry.retzer@gmail.com>
2024-11-07 19:04:13 -0600

    Created group deletion page, added different group views for group owner vs non-member, also fixed random bugs

--- foraging_app/views/group_view.py
+++ foraging_app/views/group_view.py
@@ -7,25 +7,61 @@ from oauthlib.openid.connect.core.exceptions import LoginRequired
 
 from foraging_app.forms.group import GroupCreateForm
 from foraging_app.models.group import Group, User_Group
-from foraging_app.forms import group
+from foraging_app.forms import group, CommentForm
 from foraging_app.models.user import User
 from django.contrib.auth.hashers import make_password
+from foraging_app.models import Marker
 
 
 class Group_View(LoginRequiredMixin,View):
     login_url = '/login/'
 
+    def rankingSort(self, member):
+        return member.rating
+
     def get(self, request, groupID):
         thisGroup = Group.objects.get(id=groupID)
         members = []
         markers = []
+        inGroup = False
+
         if thisGroup is not None:
             members = User_Group.getGroupMembers(None, thisGroup)
+            members.sort(key=self.rankingSort, reverse=True)
             markers = User_Group.getGroupMarkers(None, thisGroup)
-
+            for x in members:
+                if x.id == request.user.id:
+                    inGroup = True
         return render(request, "groups/group.html", {'thisGroup': thisGroup,
                                               'members': members,
-                                              'markers': markers})
+                                              'markers': markers,
+                                              'form': CommentForm(),
+                                              'inGroup': inGroup})
+
+    def post(self, request, groupID):
+        thisGroup = Group.objects.get(id=groupID)
+        if thisGroup.isPrivate:
+            # TODO: set up request system for private groups
+            messages.success(request, "Requested to join " + thisGroup.name)
+            return redirect('group', groupID)
+        else:
+            User_Group.objects.create(group_id=thisGroup, user_id=request.user)
+            messages.success(request, "Successfully joined " + thisGroup.name)
+            return redirect('group', groupID)
+
+
+
+class AddCommentGroupView(View):
+    def post(self, request, marker_id, groupID):
+        marker = Marker.objects.get(id=marker_id)
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.marker = marker
+            comment.user = request.user
+            comment.save()
+        return redirect('group', groupID)
+
 
 class Create_Group_View(LoginRequiredMixin,View):
     login_url = '/login/'
@@ -43,13 +79,7 @@ class Create_Group_View(LoginRequiredMixin,View):
             User_Group.objects.create(group_id=group_create, user_id=request.user)
             if group_create is not None:
                 messages.success(request,"Group creation complete.")
-                members = []
-                markers = []
-                members = User_Group.getGroupMembers(None, group_create)
-                markers = User_Group.getGroupMarkers(None, group_create)
-                return render(request, "groups/group.html", {'thisGroup': group_create,
-                                              'members': members,
-                                              'markers': markers})
+                return redirect('group', group_create.id)
             else:
                 messages.error(request, "Group failed to create.")
 
@@ -80,9 +110,26 @@ class Group_Edit_View(LoginRequiredMixin,View):
         thisGroup.name = request.POST['name']
         thisGroup.description = request.POST['description']
         if request.POST['isPrivate'] == 'Public':
-            thisGroup.is_private = False
+            thisGroup.isPrivate = False
         else:
-            thisGroup.is_private = True
+            thisGroup.isPrivate = True
         thisGroup.save()
 
         return redirect('group', thisGroup.id)
+
+
+class Group_Delete_View(LoginRequiredMixin,View):
+    login_url = '/login/'
+
+    def get(self, request, groupID):
+        thisGroup = Group.objects.get(id=groupID)
+        return render(request, "groups/group_delete_confirmation.html", {'thisGroup': thisGroup})
+
+    def post(self, request, groupID):
+        thisGroup = Group.objects.get(id=groupID)
+        thisGroup.delete()
+        messages.success(request, "Group deletion complete.")
+        return redirect('group_nav')
+
+
+
