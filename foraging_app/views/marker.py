86e90a8f2ee4aa5517074452ba1cda2eb3cad83d
flickin4 <flickin4@uwm.edu>
2024-12-02 19:21:03 -0600

    Styling

--- foraging_app/views/marker.py
+++ foraging_app/views/marker.py
@@ -63,13 +63,13 @@ class Marker_Create_View(LoginRequiredMixin, View):
         if marker_form.is_valid():
             data = marker_form.cleaned_data
             # TODO : Resize Image before save -> Save space.
-            image = Image.open(request.FILES['image'])
+            # image = Image.open(request.FILES['image'])
             if species_form.is_valid():
                 species_cleaned = species_form.cleaned_data
                 species = Species.objects.create(**species_cleaned)
                 data['species'] = species
-            image = image.resize(size=(400,400))            
-            data['image'] = image
+            # image = image.resize(size=(400,400))            
+            # data['image'] = image
             marker_create = Marker.objects.create(**data, owner=request.user)
             if marker_create is not None:
                 messages.success(request,"Marker created complete.")
