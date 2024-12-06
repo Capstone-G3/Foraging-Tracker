33d16a754b7668c5bcab38424d443365670bea83
danzzu <danhle346@gmail.com>
2024-12-05 19:05:20 -0600

    Refactor to use global list instead depends on users.

--- foraging_app/forms/species.py
+++ foraging_app/forms/species.py
@@ -1,12 +1,21 @@
-from django.forms import ModelForm, Textarea
+from django.forms import ModelForm, Select, CharField
 
 from foraging_app.models import Species
 
+CLASSIFICATION = [
+    ('ANIMAL','Animal'),
+    ('PLANT','Plant'),
+    ('TREE','Tree'), 
+    ('FRUIT','Fruit')]
+
 class SpeciesCreateForm(ModelForm):
+    breed = CharField(required=False)
+    
     class Meta:
         model = Species
-        fields = ['name', 'category', 'scope','description']
-        exclude = ['image']
-        widgets= [
-            {'description' : Textarea()}
-        ]
+        fields = ['type_animal', 'category', 'breed']
+        widgets = {
+            'type_animal' : Select(choices=CLASSIFICATION),
+            'category' : Select
+        }
+        
